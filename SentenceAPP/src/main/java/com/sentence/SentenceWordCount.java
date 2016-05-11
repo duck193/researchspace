a0b954d75bf2d2544a398f8b135a7ec1aa8f5beb
joe.richard <joe.richard@D43194.ulinedm.com>
2016-05-11 15:46:11 -0500

    # WARNING: head commit changed in the meantime
    Initial upload of SentenceAPP java files.

--- SentenceAPP/src/main/java/com/sentence/SentenceWordCount.java
+++ SentenceAPP/src/main/java/com/sentence/SentenceWordCount.java
@@ -1,12 +1,33 @@
 package com.sentence;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class SentenceWordCount {
-
+	
+	
+	private static SentenceWordCount sentenceWordCountObject;
 	private static String sentence = "This is a new way in to count how many times one string, can have two#two or more three!three!three numbers written in four*four*four*four.";
 	
-	public static void main(String[] args) {
+	/**
+	 * GetSentenceWordCount
+	 **
+	 * Return a SentenceWordCount object
+	 * @return SentenceWordCount
+	 */
+	public static SentenceWordCount getSentenceWordCount() {
+		if(sentenceWordCountObject==null) {
+			sentenceWordCountObject = new SentenceWordCount();
+			return sentenceWordCountObject;
+		}
+		else {
+			return sentenceWordCountObject;
+		}
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public String[] mainObject(String[] args) {
+		ArrayList al = new ArrayList<String>();
 		CountWords cw;
 		if(args.length>0) {
 			cw = new CountWords(args);
@@ -18,13 +39,26 @@ public class SentenceWordCount {
 		Map<String, Integer> wordsOriginalOrder = cw.getWords();
 
 		// Key Sort Order
-		System.out.println("Alphanumeric Key Sort");
+		al.add("Alphanumeric Key Sort");
 		Map<String, Integer> wordsKeySortedOrder = cw.mapSorter(wordsOriginalOrder, "Key");
-		wordsKeySortedOrder.forEach((k,v) -> System.out.println(k + " : " + v));
+		wordsKeySortedOrder.forEach((k,v) -> al.add(k + " : " + v));
 		
 		// Value Sort Order
-		System.out.println("\nNumerical Value Sort");
+		al.add("\nNumerical Value Sort");
 		Map<String, Integer> wordsValueSortedOrder = cw.mapSorter(wordsOriginalOrder, "Value");
-		wordsValueSortedOrder.forEach((k,v) -> System.out.println(k + " : " + v));
+		wordsValueSortedOrder.forEach((k,v) -> al.add(k + " : " + v));
+		
+		return (String[])al.toArray();
+	}
+	
+	public void doStaticCall(String[] parameter) {
+		SentenceWordCount.main(parameter);
+	}
+	
+	public static void main(String[] args) {
+		SentenceWordCount swc = new SentenceWordCount();
+		for (String arrayElement : swc.mainObject(args)) {
+			System.out.println(arrayElement);
+		}
 	}
 }
