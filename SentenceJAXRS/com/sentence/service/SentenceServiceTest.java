188205a1c5fe88e043886ddbc40b81be55b893dc
joe.richard <joe.richard@D43194.ulinedm.com>
2016-05-13 14:46:43 -0500

    Added Unit Test SenteceServiceTest.java

---
+++ SentenceJAXRS/com/sentence/service/SentenceServiceTest.java
@@ -0,0 +1,11 @@
+package com.sentence.service;
+
+import junit.framework.TestCase;
+
+public class SentenceServiceTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
