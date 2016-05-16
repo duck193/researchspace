8efac83f854422acc9c2583d324554d5315359c2
joe.richard <joe.richard@D43194.ulinedm.com>
2016-05-16 13:06:38 -0500

    Added service for sorted map.

--- SentenceJAXRS/src/main/java/com/sentence/service/SentenceService.java
+++ SentenceJAXRS/src/main/java/com/sentence/service/SentenceService.java
@@ -7,17 +7,56 @@ import com.ibm.json.java.JSONObject;
 import com.sentence.CountWords;
 
 @Path("/sentenceservice")
-@Produces({ "application/xml", "application/json" })
-@Consumes({ "application/xml", "application/json" })
+@Produces("application/json")
 public class SentenceService {
 	
-	@Path("{sentence}")
-	@POST
+	/**
+	 * GetUnsortedSentence
+	 **
+	 * Return unsorted sentence results
+	 * @param sentence
+	 * @return JSON
+	 */
+	@Path("/unsorted")
+	@GET
 	@Produces("application/json")
-	public Response getUnsortedSentence(@PathParam("sentence") String sentence) {
-		JSONObject jsonObject = new JSONObject();
-		CountWords cw = new CountWords(sentence);
-	  cw.getWords().forEach((k,v) -> jsonObject.put(k, v));
-		return Response.status(200).entity(jsonObject).build();
+	public Response getUnsortedSentence(@QueryParam("sentence") String sentence) {
+		if (sentence!=null) {
+			JSONObject jsonObject = new JSONObject();
+			CountWords cw = new CountWords(sentence);
+		  cw.getWords().forEach((k,v) -> jsonObject.put(k, v));
+			return Response.status(200).entity(jsonObject).build();
+		}
+		else {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("MissingParameter", "Sentence parameter was not passed");
+			return Response.status(400).entity(jsonObject).build();
+		}
+	}
+	
+	/**
+	 * GetSortedSentence
+	 **
+	 * Return sorted sentence results
+	 * @param sentence
+	 * @param sorttype
+	 * @return JSON
+	 */
+	@Path("/sorted")
+	@GET
+	@Produces("application/json")
+	public Response getSortedSentence(@QueryParam("sentence") String sentence,
+																		@QueryParam("sorttype") @DefaultValue("KEY") String sortType) {
+		if (sentence!=null) {
+			JSONObject jsonObject = new JSONObject();
+			CountWords cw = new CountWords(sentence);
+		  cw.mapSorter(cw.getWords(), sortType).forEach((k,v) -> jsonObject.put(k, v));
+			return Response.status(200).entity(jsonObject).build();
+		}
+		else {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("MissingParameter", "Sentence parameter was not passed");
+			return Response.status(400).entity(jsonObject).build();	
+		}
 	}
 }
