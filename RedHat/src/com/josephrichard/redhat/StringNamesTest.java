9a59ea7efe543733c767cc1932d2e87eaf50b02f
joe.richard <joe.richard@D43194.ulinedm.com>
2016-05-16 10:03:58 -0500

    Converted project to Maven and added new test case

--- RedHat/src/com/josephrichard/redhat/StringNamesTest.java
+++ RedHat/src/com/josephrichard/redhat/StringNamesTest.java
@@ -2,18 +2,35 @@ package com.josephrichard.redhat;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class StringNamesTest {
 
-	@Before
-	public void setUp() throws Exception {
+	private String[] itemsToAdd = {"This", "is", "what", "a", "String", "Array", "looks", "like"};
+	private StringNames sn = new StringNames();
+	
+	@Test
+	public void stringBufferTest() {
+		String bufferOutput = sn.addStringItems(itemsToAdd, false);
+		assertTrue(bufferOutput.equals("ThisiswhataStringArraylookslike"));
+	}
+	
+	@Test
+	public void stringBufferUpperCaseTest() {
+		String bufferOutput = sn.addStringItems(itemsToAdd, true);
+		assertTrue(bufferOutput.equals(("ThisiswhataStringArraylookslike").toUpperCase()));		
+	}
+	
+	@Test
+	public void arrayListTest() {
+		String listOutput = sn.addStringItemsCollectionsFramework(itemsToAdd, false);
+		assertTrue(listOutput.equals("ThisiswhataStringArraylookslike"));
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void arrayListUpperCaseTest() {
+		String listOutput = sn.addStringItemsCollectionsFramework(itemsToAdd, true);
+		assertTrue(listOutput.equals(("ThisiswhataStringArraylookslike").toUpperCase()));
 	}
 
 }
